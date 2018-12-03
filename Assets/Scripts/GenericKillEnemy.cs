6b520aa9ed365aae4339804755456cfe9cd6f7a8
Sebastian Eking <sebastian.eking@elev.ga.lbs.se>
2018-12-03 10:45:53 +0100

    Kommentrade och började lägga till "life"

---
+++ Assets/Scripts/GenericKillEnemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenericKillEnemy : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Destroy(transform.parent.gameObject); //förstör collidern vid toppen av fiende objektet och dens parent när denna collidern nuddar "Player"
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,8 +6,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [Range(0, 20f)]
-    public float movespeed;
-    public float jumpHeight;
+    public float movespeed; //en variabel för hur snabbt playern rör sig
+    public float jumpHeight; //en variabel för hur högt playern kan hoppa
 
 
     public GroundCheck groundCheck;
@@ -24,11 +24,10 @@ public class PlayerMovement : MonoBehaviour {
 	void Update ()
     {
 
-        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movespeed, rbody.velocity.y);
-
+        rbody.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * movespeed, rbody.velocity.y); //spelarens y axel ändras så att spelar rör på sig
         if(Input.GetKeyDown(KeyCode.Space))
         {
-            if (groundCheck.touches > 0) // om touches är positiv så kan man inte hoppa alltså när man är i luften
+            if (groundCheck.touches > 0) // om touches är positiv så kan man inte hoppa, alltså när man är i luften
             {
             rbody.velocity = new Vector2(rbody.velocity.x, jumpHeight); //ändrar players y med jumpheight
             }
